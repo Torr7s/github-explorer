0086f9e8320971fc068383a37dac85b9bcfebd6d
Torr7s <torresmiranda.rj@gmail.com>
2022-06-07 20:52:39 -0300

    Setup for typescript usage

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    ['@babel/preset-react', {
-      /* Allows the non-import of react in every .jsx file */
-      runtime: 'automatic'
-    }]
+    '@babel/preset-typescript',
+    [
+      '@babel/preset-react', {
+        /* Allows the non-import of react in every .jsx file */
+        runtime: 'automatic'
+      }
+    ]
   ]
 }
