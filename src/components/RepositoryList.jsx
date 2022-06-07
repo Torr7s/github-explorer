5736517c4ae1fef1c1762fd81cd62951bd11c563
Torr7s <torresmiranda.rj@gmail.com>
2022-06-07 12:05:54 -0300

    Work with useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import '../styles/repositories.scss';
 
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 const repository = {
@@ -9,6 +10,26 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  /**
+   * useEffect Will trigger a function as soon as something happens in the app, 
+   * such as changing the value of a variable.
+   * 
+   * It receives TWO parameters: 
+   *  1. The function to be executed 
+   *  2. When it will be executed
+   *   ↳ Also known as dependencies;
+   *   ↳ If an empty array is passed in, the function within useEffect will be executed only once
+   *     as soon as the component is displayed;
+  */
+  useEffect(() => {
+    /* first param */
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, /* second param */[]);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
