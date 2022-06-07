183abe7348566b9a553d0fb128660bec649df5ca
Torr7s <torresmiranda.rj@gmail.com>
2022-06-07 10:24:09 -0300

    Create second component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './styles/global.scss';
 
+import { RepositoryList } from './components/RepositoryList';
+
 export function App() {
   return (
-    <h1>
-      Hello, World!
-    </h1>
+    <RepositoryList />
   );
 }
